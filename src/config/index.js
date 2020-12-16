e80f262d596c2cead2cbcdc0a1e118a733c5083e
VentumSoftware <jbnogal@ventum-software.com>
2020-12-16 14:09:27 -0300

    token virgen

--- src/config/index.js
+++ src/config/index.js
@@ -12,7 +12,7 @@ let vars = {
     ADNGitUser: process.env.ADN_GIT_USER || 'VentumSoftware', // <ADN-GIT-USER>
     ADNGitRepo: process.env.ADN_GIT_REPO || 'ADN-Masterbus-IOT', // <ADN-GIT-USER-REPO>
     //ADNGitAuthToken: process.env.ADN_GIT_AUTH_TOKEN || '29d84d785cd8c440b311721816cb7f8855e51983', // <AUTH-TOKEN>
-    ADNGitAuthToken: '98511f660bc649f4c1701010a3222faf55fc041b',
+    ADNGitAuthToken: '962e57a62a7289811ed0bd32bf92a679dd6e7b02',
     //Encryptacion JWT
     //TODO: Esto debería estar acá o en el adn?
     jwtSecret: process.env.JWT_SECRET || "YOUR_secret_key", // key privada que uso para hashear passwords
