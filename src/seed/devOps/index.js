63bbcb4d37c930a833b65f732c959535b5c376db
VentumSoftware <jbnogal@ventum-software.com>
2021-02-10 12:45:36 -0300

    middleware para devops

--- src/seed/devOps/index.js
+++ src/seed/devOps/index.js
@@ -21,6 +21,28 @@ const init = async (env, reset) => {
   const app = express();
   const port = env.devOpsPORT;
 
+  const multer = require('multer');
+    const upload = multer({ dest: 'public/uploads/' });
+    const cookieParser = require('cookie-parser') // Herramienta para parsear las cookies
+    const bodyParser = require('body-parser'); // Herramienta para parsear el "cuerpo" de los requests
+    const morgan = require('morgan'); // Herramienta para loggear
+    //Middlewares:
+
+    //"Morgan" es una herramienta para loggear
+    app.use(morgan(':method :url :status :res[content-length] - :response-time ms'));
+    console.log(`endpoints@setup: 'morgan' middleware aagregado`);
+    app.use(cookieParser());
+    console.log(`endpoints@setup: 'cookieParser' middleware agregado`);
+    //"bodyParser" es un middleware que me ayuda a parsear los requests
+    app.use(bodyParser.urlencoded());
+    console.log(`endpoints@setup: 'bodyParser.urlencoded' middleware agregado`);
+    app.use(bodyParser.json());
+    console.log(`endpoints@setup: 'bodyParser.json' middleware agregado`);
+    // Esto lo hago para devolver el favicon.ico
+    //TODO: ver q es el favicon y si es necesario esto
+    //app.use(favicon(path.join(__dirname, '../../public/assets/icons', 'favicon.ico')));
+    // Agrego una función que me devuelve la URL que me resulta cómoda
+
   //TODO: Hacer algo mas prolijo y seguro que esto...
   app.post('/reset', async (req, res) => {
 
