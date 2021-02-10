b4591a0316446348d3fc05503374107ce2e6f01b
VentumSoftware <jbnogal@ventum-software.com>
2021-02-10 12:48:11 -0300

    log de devops

--- src/seed/devOps/index.js
+++ src/seed/devOps/index.js
@@ -14,34 +14,34 @@ const init = async (env, reset) => {
           console.log(`App: DevOps listening in PORT:  ${port}`);
           resolve();
         }
-      });     
+      });
     });
-  } 
+  }
 
   const app = express();
   const port = env.devOpsPORT;
 
   const multer = require('multer');
-    const upload = multer({ dest: 'public/uploads/' });
-    const cookieParser = require('cookie-parser') // Herramienta para parsear las cookies
-    const bodyParser = require('body-parser'); // Herramienta para parsear el "cuerpo" de los requests
-    const morgan = require('morgan'); // Herramienta para loggear
-    //Middlewares:
+  const upload = multer({ dest: 'public/uploads/' });
+  const cookieParser = require('cookie-parser') // Herramienta para parsear las cookies
+  const bodyParser = require('body-parser'); // Herramienta para parsear el "cuerpo" de los requests
+  const morgan = require('morgan'); // Herramienta para loggear
+  //Middlewares:
 
-    //"Morgan" es una herramienta para loggear
-    app.use(morgan(':method :url :status :res[content-length] - :response-time ms'));
-    console.log(`endpoints@setup: 'morgan' middleware aagregado`);
-    app.use(cookieParser());
-    console.log(`endpoints@setup: 'cookieParser' middleware agregado`);
-    //"bodyParser" es un middleware que me ayuda a parsear los requests
-    app.use(bodyParser.urlencoded());
-    console.log(`endpoints@setup: 'bodyParser.urlencoded' middleware agregado`);
-    app.use(bodyParser.json());
-    console.log(`endpoints@setup: 'bodyParser.json' middleware agregado`);
-    // Esto lo hago para devolver el favicon.ico
-    //TODO: ver q es el favicon y si es necesario esto
-    //app.use(favicon(path.join(__dirname, '../../public/assets/icons', 'favicon.ico')));
-    // Agrego una función que me devuelve la URL que me resulta cómoda
+  //"Morgan" es una herramienta para loggear
+  app.use(morgan(':method :url :status :res[content-length] - :response-time ms'));
+  console.log(`endpoints@setup: 'morgan' middleware aagregado`);
+  app.use(cookieParser());
+  console.log(`endpoints@setup: 'cookieParser' middleware agregado`);
+  //"bodyParser" es un middleware que me ayuda a parsear los requests
+  app.use(bodyParser.urlencoded());
+  console.log(`endpoints@setup: 'bodyParser.urlencoded' middleware agregado`);
+  app.use(bodyParser.json());
+  console.log(`endpoints@setup: 'bodyParser.json' middleware agregado`);
+  // Esto lo hago para devolver el favicon.ico
+  //TODO: ver q es el favicon y si es necesario esto
+  //app.use(favicon(path.join(__dirname, '../../public/assets/icons', 'favicon.ico')));
+  // Agrego una función que me devuelve la URL que me resulta cómoda
 
   //TODO: Hacer algo mas prolijo y seguro que esto...
   app.post('/reset', async (req, res) => {
@@ -55,10 +55,11 @@ const init = async (env, reset) => {
       var msg = "DevOps: App restarted succesfully!"
 
       await reset().catch(err => {
-        msg = "DevOps App restarting error: " + err;
-      }) 
+        console.log(err);
+        msg = "DevOps App failed to restart!";
+      })
       console.log(msg);
-      res.status(200).send(msg);  
+      res.status(200).send(msg);
 
     } else {
       console.log("DevOps: failed RESET " + req.body);
